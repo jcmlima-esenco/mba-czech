b6f6538ea66b6990602a467ee428149e820cd69d
Julio Cesar de Melhado e Lima <41648284+jcmlima-esenco@users.noreply.github.com>
2018-11-23 15:23:31 -0200

    new version

---
+++ exemplo mysql.R
@@ -0,0 +1,31 @@
+# Testando ler do MySql
+
+install.packages("RMySQL")
+library(RMySQL)
+
+con <- dbConnect(MySQL(), user='czech', password='czech123', dbname='czech', host='czech-mysql.ctidba6hrq5v.us-east-1.rds.amazonaws.com')
+
+dbListTables(con)
+dbWriteTable(con, "mtcars", mtcars)
+dbListTables(con)
+
+dbListFields(con, "mtcars")
+dbReadTable(con, "mtcars")
+
+# You can fetch all results:
+res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
+dbFetch(res)
+dbClearResult(res)
+
+# Or a chunk at a time
+res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
+while(!dbHasCompleted(res)){
+  chunk <- dbFetch(res, n = 5)
+  print(nrow(chunk))
+}
+# Clear the result
+dbClearResult(res)
+
+# Disconnect from the database
+dbDisconnect(con)
+
--- read and tidy.R
+++ read and tidy.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+library(lubridate)
 
 convert_from_czech <- function(original) {
   case_when(
@@ -22,9 +24,19 @@ convert_from_czech <- function(original) {
 }
 
 birth_day <- function(birth_number) {
-  as.integer( substr(birth_number, 3, 4) )
+  as.integer( substr(birth_number, 5, 6) )
 }
 
+birth_month <- function(birth_number) {
+  month <- as.integer( substr(birth_number, 3, 4) )
+  if ( month > 12) month = month - 50
+  month
+}
+
+birth_year <- function(birth_number) {
+  year <- as.integer( substr(birth_number, 1, 2) )
+  year + 1900
+}
 
 decode_gender <- function(birth_day) {
   ifelse( birth_day >= 50 , 
@@ -35,21 +47,19 @@ decode_gender <- function(birth_day) {
 
 decode_birth_date <- function(gender, birth_number) {
   if_else(gender == "Male",
-        as.Date.character(as.character(birth_number),"%Y%m%d"),
-        as.Date.character(as.character(birth_number-5000),"%Y%m%d")
+        as.Date.character(as.character(birth_number),"%y%m%d"),
+        as.Date.character(as.character(birth_number-5000),"%y%m%d")
   )
 }
 
 transactions <- read.csv2("data/trans.asc", stringsAsFactors = FALSE)
 
-transactions <- transactions %>% mutate(
-  amount = as.double(amount),
-  balance = as.double(balance),
-  date = as.Date.character(date,"%y%m%d"),
-  type = convert_from_czech(type),
-  operation = convert_from_czech(operation)
-)
-
+transactions <- transactions %>% mutate(amount = as.double(amount)) %>%
+  mutate(balance = as.double(balance)) %>%
+  mutate(date = as.Date.character(date,"%y%m%d")) %>%
+  mutate(type = convert_from_czech(type)) %>%
+  mutate(operation = convert_from_czech(operation)) 
+         
 transactions %>% count(type)
 
 transactions %>% count(operation)
@@ -69,14 +79,31 @@ clients <- read.csv2("data/client.asc", stringsAsFactors = FALSE)
 
 clients <- clients %>% mutate(
   gender = decode_gender(birth_day(birth_number)),
-  birth_date = decode_birth_date(birth_number)
+  birth_date = decode_birth_date(gender,birth_number)
 )
 
+disp <- read.csv2("data/disp.asc", stringsAsFactors = FALSE)
+
 districts <- read.csv2("data/district.asc", stringsAsFactors = FALSE)
-districts <- 
+ 
+mmm <- districts %>% left_join(clients, by= c("A1" = "district_id"))  %>%
+    left_join(disp, by = c("client_id.y" = "client_id" )) %>%
+    left_join(accounts, by = c("account_id" = "account_id" )) %>%
+    left_join(transactions, by.x = "account_id", by.y = "account_id"))
 
-mmm <- districts %>% left_join(clients, by= c("A1" = "district_id"))
 
+transactions %>% ggplot(aes(x=type, y=amount)) + geom_boxplot()
 
+transactions %>% ggplot(aes(x=amount)) + geom_histogram()
+
+transactions %>% ggplot(aes(x=round_date(date, unit = "month"), y=amount)) + geom_dotplot()
+
+transactions %>% ggplot(aes(x=type)) + geom_bar(aes(y=sum(amount)))
+
+transactions %>% ggplot(aes(x=wday(date), y=amount, group=wday(date))) + geom_boxplot()
+
+transactions %>% ggplot(aes(x=day(date), y=amount, group=day(date))) + geom_boxplot()
+
+transactions %>% ggplot(aes(x=type, y=amount)) + geom_bar(stat="sum")
 
 
